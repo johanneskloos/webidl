82b943683520825109fd4c5777c70050383c8c60
jkloos <jkloos@mpi-sws.org>
2016-06-01 17:20:59 +0200

    Provide for user attributes.

--- idlData.mli
+++ idlData.mli
@@ -30,7 +30,21 @@ type types =
   | OptionType of bool * types
   | NullableType of types
   | SequenceType of types
-type value = Ast.value
+type value = Ast.value =
+  | StringValue of string
+  | BoolValue of bool
+  | FloatValue of float
+  | IntValue of int
+  | NullValue
+
+type argument_kind = Single | Multiple | Optional | Default of value
+type argument = { name : string; types : types; kind : argument_kind; user_attributes: user_attribute list }
+and user_attribute =
+  | UAPlain of string
+  | UAEquals of string * string
+  | UAArguments of string * argument list
+  | UAArgumentsEquals of string * string * argument list
+
 type inheritance_mode = Toplevel | InheritsFrom of string | ArrayClass
 type special_handling = {
   this_lenient : bool;
@@ -38,7 +52,12 @@ type special_handling = {
   named_properties_object : bool;
   override_builtins : bool;
 }
-type constant = { name : string; types : types; value : value; }
+type constant = {
+  name : string;
+  types : types;
+  value : value;
+  user_attributes: user_attribute list
+}
 type access =
     ReadWrite
   | ReadOnly
@@ -51,22 +70,22 @@ type attribute = {
   lenient_this : bool;
   inherited : bool;
   access : access;
+  user_attributes: user_attribute list
 }
-type argument_kind = Single | Multiple | Optional | Default of value
-type argument = { name : string; types : types; kind : argument_kind; }
-type operation = { name : string; return : types; args : argument list; }
-type legacy_caller = { return : types; args : argument list; }
+type operation = { name : string; return : types; args : argument list; user_attributes: user_attribute list }
+type legacy_caller = { return : types; args : argument list; user_attributes: user_attribute list }
+type attributed_type = { types: types; user_attributes: user_attribute list }
 type property_set = {
-  getter : types option;
-  deleter : types option;
-  setter : (types * types) option;
-  creator : (types * types) option;
+  getter : attributed_type option;
+  deleter : attributed_type option;
+  setter : (attributed_type * attributed_type) option;
+  creator : (attributed_type * attributed_type) option;
 }
 type stringifer_mode =
     NoStringifier
-  | InternalStringifer of string_behavior
-  | AttributeStringifier of string * string_behavior
-type constructor = { name : string; args : argument list; }
+  | InternalStringifer of string_behavior * user_attribute list
+  | AttributeStringifier of string * string_behavior * user_attribute list
+type constructor = { name : string; args : argument list; user_attributes: user_attribute list }
 type interface = {
   inheritance_mode : inheritance_mode;
   name : string;
@@ -81,31 +100,36 @@ type interface = {
   legacy_callers : legacy_caller list;
   not_exposed : bool;
   stringifier : stringifer_mode;
+  user_attributes: user_attribute list
 }
 type dictionary_entry = {
   name : string;
   types : types;
   default_value : value option;
+  user_attributes: user_attribute list
 }
 type dictionary = {
   name : string;
   inherits_from : string option;
   members : dictionary_entry list;
+  user_attributes: user_attribute list
 }
-type exception_member = { name : string; types : types; }
+type exception_member = { name : string; types : types; user_attributes: user_attribute list }
 type exception_ = {
   name : string;
   inherits_from : string option;
   consts : constant list;
   members : exception_member list;
   not_exposed : bool;
+  user_attributes: user_attribute list
 }
-type enumerate = { name : string; values : string list; }
+type enumerate = { name : string; values : string list; user_attributes: user_attribute list }
 type callback = {
   name : string;
   return : types;
   args : argument list;
   treat_non_callable_as_null : bool;
+  user_attributes: user_attribute list
 }
 type definitions = {
   dictionaries : dictionary StringMap.t;
