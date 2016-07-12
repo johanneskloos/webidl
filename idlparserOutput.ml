9c78ffcc7c25b50507aac83e5bba34f5e56cf839
jkloos <jkloos@mpi-sws.org>
2016-07-12 17:56:25 +0200

    Add IDL parser debugging messages.

---
+++ idlparserOutput.ml
@@ -0,0 +1,278 @@
+
+(* This file was auto-generated based on "idlparserOutput.messages". *)
+
+(* Please note that the function [message] can raise [Not_found]. *)
+
+let message =
+  fun s ->
+    match s with
+    | 233 ->
+        "Expected `;' to finish the exception definition.\n"
+    | 142 ->
+        "Expected `;' to finish the operation definition.\n"
+    | 136 ->
+        "Expected further exception names, or `)'.\n"
+    | 134 ->
+        "Expected a list of exception names.\n"
+    | 0 ->
+        "Expected definitions.\n"
+    | 1 ->
+        "Expected an identifer or DOMString in the valuetype definition.\n"
+    | 53 ->
+        "Expected `;' to finish the valuetype definition, or a type expression suffix.\n"
+    | 10 ->
+        "Expected a type expression in valuetype.\n"
+    | 3 ->
+        "Expected the typedef expression `valuetype DOMString sequence<unsigned short>'.\n"
+    | 4 ->
+        "Expected the typedef expression `valuetype DOMString sequence<unsigned short>'.\n"
+    | 5 ->
+        "Expected the typedef expression `valuetype DOMString sequence<unsigned short>'.\n"
+    | 6 ->
+        "Expected the typedef expression `valuetype DOMString sequence<unsigned short>'.\n"
+    | 7 ->
+        "Expected the typedef expression `valuetype DOMString sequence<unsigned short>'.\n"
+    | 8 ->
+        "Expected a `;' or a type expression suffix to finish the valuetype definition.\n"
+    | 112 ->
+        "Expected an identifier or `DOMString'.\n"
+    | 115 ->
+        "Expected `;' to finish the typedef expression.\n"
+    | 113 ->
+        "Expected `;' to finish the typedef expression.\n"
+    | 55 ->
+        "Expected extended attributes or a type expression.\n"
+    | 56 ->
+        "Expected a list of extended attributes.\n"
+    | 58 ->
+        "Expected `=' followed by an identifier, an argument list, `]' or `,' followed by further\nextended attributes.\n"
+    | 111 ->
+        "Expected a type expression.\n"
+    | 96 ->
+        "Expected `...', an argument name, or a type expression suffix.\n"
+    | 101 ->
+        "Expected further arguments, or `)'.\n"
+    | 106 ->
+        "Expected further extended attributes, or ']'.\n"
+    | 102 ->
+        "Expected further arguments, or `)'.\n"
+    | 98 ->
+        "Expected an argument name.\n"
+    | 62 ->
+        "Expected a list of arguments.\n"
+    | 71 ->
+        "Expected an argument name.\n"
+    | 81 ->
+        "Expected a default value, ',' followed by further arguments, or ')'.\n"
+    | 70 ->
+        "Expected a type for the argument.\n"
+    | 67 ->
+        "Expected `in', or an argument specification.\n"
+    | 69 ->
+        "Expected an argument specification.\n"
+    | 59 ->
+        "Expected an identifier.\n"
+    | 61 ->
+        "Expected an argument list, `,' followed by further extended attributes, or `]'.\n"
+    | 107 ->
+        "Expected an extended attribute, or `]' to finish the list of extended attributes.\n"
+    | 117 ->
+        "Expected `dictionary' or `interface'.\n"
+    | 118 ->
+        "Expected an identifier for the partial interface.\n"
+    | 119 ->
+        "Expected a `{' to start the partial interface body.\n"
+    | 120 ->
+        "Expected a list of interface members for the partial interface definition.\n"
+    | 124 ->
+        "Expected a `;' to finish the partial interface definition.\n"
+    | 190 ->
+        "Expected an identifier for the partial dictionary.\n"
+    | 191 ->
+        "Expected a `{' to start the dictionary body.\n"
+    | 192 ->
+        "Expected a list of dictionary members.\n"
+    | 203 ->
+        "Expected a `;' to finish the partial dictionary definition.\n"
+    | 205 ->
+        "Expected a module identifier.\n"
+    | 206 ->
+        "Expected `{' followed by a module body.\n"
+    | 207 ->
+        "Expected definitions.\n"
+    | 274 ->
+        "Expected further definitions, or `}' to finish the module.\n"
+    | 208 ->
+        "Expected an interface identifier.\n"
+    | 209 ->
+        "Expected an inheritance specification, an interface body (marked by `{') or a forward declaration (marked by `;').\n"
+    | 277 ->
+        "Expected further definitions.\n"
+    | 282 ->
+        "Expected further definitions.\n"
+    | 130 ->
+        "Expected an operation identifier or an argument list.\n"
+    | 132 ->
+        "Expected a raises specification or `;' to finish the operation definition.\n"
+    | 133 ->
+        "Expected `{' followed by a list of exception names.\n"
+    | 131 ->
+        "Expected an argument specification.\n"
+    | 215 ->
+        "Expected a list of interface members.\n"
+    | 127 ->
+        "Expected a strinigifier declaration.\n"
+    | 185 ->
+        "Expected further interface members, or `}' to finish the interface definition.\n"
+    | 182 ->
+        "Expected an operation specification.\n"
+    | 149 ->
+        "Expected `attribute'.\n"
+    | 217 ->
+        "Expected a `;' to finish the interface definition.\n"
+    | 126 ->
+        "Expected an interface member.\n"
+    | 147 ->
+        "Expected `readonly' or `attribute'.\n"
+    | 151 ->
+        "Expected an identifier for the attribute.\n"
+    | 152 ->
+        "Expected a getter specification, setter specification or `;'.\n"
+    | 160 ->
+        "Expected `{' to start the list of exception names.\n"
+    | 161 ->
+        "Expected a list of exception names.\n"
+    | 164 ->
+        "Expected `;' to finish the attribute definition.\n"
+    | 162 ->
+        "Expected `}' to finish the setRaises specification, or further exception names.\n"
+    | 153 ->
+        "Expected `getter'.\n"
+    | 159 ->
+        "Expected a setRaises specification, or `;' to finish the attribute definition.\n"
+    | 155 ->
+        "Expected `{' to start the list of exception names.\n"
+    | 156 ->
+        "Expected a list of exception names.\n"
+    | 157 ->
+        "Expected `}' to finish the getRaises specification, or further exception names.\n"
+    | 150 ->
+        "Expected type for the attribute.\n"
+    | 211 ->
+        "Expected a list of interfaces to inherit from.\n"
+    | 138 ->
+        "Expected interface body - forward declaration cannot provide inheritance.\n"
+    | 214 ->
+        "Expected `{' to start the interface body.\n"
+    | 139 ->
+        "Expected further names to inherit from, or a continuation of the enclosing statement.\n"
+    | 36 ->
+        "Not a valid scoped name.\n"
+    | 261 ->
+        "Expected `implements' for the implements definition.\n"
+    | 262 ->
+        "Expected a scoped name on the LHS of the implements definition.\n"
+    | 263 ->
+        "Expected `;' to finish the implements definition.\n"
+    | 37 ->
+        "Expect further identifiers after `::'.\n"
+    | 219 ->
+        "Expected an identifier for the exception.\n"
+    | 220 ->
+        "Expected an inheritance specification, or a `{' to start the exception body.\n"
+    | 226 ->
+        "Expected an identifier for the exception field, or a type expression suffix.\n"
+    | 227 ->
+        "Expected a `;' to finish the exception member definition.\n"
+    | 229 ->
+        "Expected further exception members, or `}' to finish the exception body.\n"
+    | 222 ->
+        "Expected a list of exception members.\n"
+    | 225 ->
+        "Expected a list of exception members.\n"
+    | 221 ->
+        "Expected `{' to start the exception body.\n"
+    | 235 ->
+        "Expected an identifier for the enumeration.\n"
+    | 236 ->
+        "Expected `{' to start the enum body.\n"
+    | 237 ->
+        "Expected a list of string as enum body.\n"
+    | 238 ->
+        "Expected further strings, or `}' to finish the enum definition.\n"
+    | 243 ->
+        "Expected a `;' to finish the enum definition.\n"
+    | 239 ->
+        "Expected a list of strings as enum body.\n"
+    | 19 ->
+        "Invalid continuation of a scoped name.\n"
+    | 22 ->
+        "Invalid continuation of a scoped name.\n"
+    | 23 ->
+        "Expect further identifiers after `::'.\n"
+    | 245 ->
+        "Expect an identifier for the dictionary definition.\n"
+    | 246 ->
+        "Expect an inheritance specification or a `{' to start the dictionary defintion body.\n"
+    | 196 ->
+        "Expect an identifier for the dictionary member, or a type expression suffix.\n"
+    | 197 ->
+        "Expected a default value, or `;' to end the dictionary member definition.\n"
+    | 200 ->
+        "Expected more dictionary members, or a `}' to close the dictionary definition body.\n"
+    | 82 ->
+        "Expected a default value in a dictionary member definition.\n"
+    | 198 ->
+        "Expected a `;' to end a dictionary member definition.\n"
+    | 248 ->
+        "Expected a list of dictionary members in a dictionary definition.\n"
+    | 250 ->
+        "Expected a `;' to end a dictionary definition.\n"
+    | 195 ->
+        "Expected a dictionary member defintion.\n"
+    | 247 ->
+        "Expected `{' starting the body of a dictionary definition.\n"
+    | 176 ->
+        "Expected constant identifier or type expression suffix in constant expression.\n"
+    | 47 ->
+        "Expected `]' in type expression.\n"
+    | 177 ->
+        "Expected `=' in constant expression.\n"
+    | 178 ->
+        "Expected value of constant in constant expression.\n"
+    | 179 ->
+        "Expected `;' to end const expression.\n"
+    | 87 ->
+        "Expected `infinity' in `-infinity'.\n"
+    | 175 ->
+        "Expected constatnt type in constant expression.\n"
+    | 40 ->
+        "Expected integer type, i.e., `short', `long' or `long long'.\n"
+    | 44 ->
+        "Expected float type, i.e., `float' or `double'.\n"
+    | 14 ->
+        "Expected `<' in sequence type expression.\n"
+    | 51 ->
+        "Expected type expression suffix or `>' to finish sequence type expression.\n"
+    | 15 ->
+        "Expected type in sequence type expression.\n"
+    | 42 ->
+        "Cannot parse integer type expression.\n"
+    | 30 ->
+        "Cannot parse type expression.\n"
+    | 32 ->
+        "Expected `or'-seperated list of types.\n"
+    | 18 ->
+        "Expected `or'-seperated list of types.\n"
+    | 252 ->
+        "Expected callback identifier in callback definition.\n"
+    | 254 ->
+        "Expected `=' to continue callback definition.\n"
+    | 256 ->
+        "Expected argument list in callback definition.\n"
+    | 257 ->
+        "Callback definitions must finish with a `;'.\n"
+    | 255 ->
+        "Expected return type in callback definition.\n"
+    | _ ->
+        raise Not_found
